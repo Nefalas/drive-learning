b51602f4a78af3b2fd7a202866c509feea22df9f
Alexis Lindgren <alexis.lindgren@hotmail.se>
2021-02-02 16:05:27 +0100

    Remove uglify

--- conf/webpack.prod.js
+++ conf/webpack.prod.js
@@ -12,17 +12,6 @@ module.exports = Merge(CommonConfig, {
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-        screw_ie8: true
-      },
-      comments: false
     })
   ]
 });
